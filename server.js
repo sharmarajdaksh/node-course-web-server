61e3c1bb706ee7616e8c5b3af29ebc7ebc2add8f
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2019-04-08 15:42:45 +0530

    Challenge Porjects page

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Portfolio page here'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Something went wrong'
